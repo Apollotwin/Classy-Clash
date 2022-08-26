f4cd6788eb24223f3a7b356049658b4db773bbfa
Simon Magnusson <simonmagnusson9205@gmail.com>
2022-08-26 16:23:33 +0200

    Load & Render map

--- Classy Clash/main.cpp
+++ Classy Clash/main.cpp
@@ -1,7 +1,27 @@
 ﻿#include "raylib.h"
 #include "raymath.h"
 
+
+
 int main()
 {
-    
+    const int windowWidth{GetScreenWidth()};
+    const int windowHeight{GetScreenHeight()};
+    InitWindow(windowWidth, windowHeight,"Classy Clash");
+
+    const Texture2D map = LoadTexture("Map/Classy_Clash_map.png");
+    const float mapScale{3.5f};
+    Vector2 mapPos{};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        DrawTextureEx(map,mapPos,0.f,mapScale,WHITE);
+
+        EndDrawing();
+    }
+    CloseWindow();
 }
